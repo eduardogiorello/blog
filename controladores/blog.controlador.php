32068bd8e57ec329061209e8d792c4d0bd989ae5
eduardo <eduardogiorello@hotmail.com>
2022-08-17 08:25:19 -0300

    subir foto

--- controladores/blog.controlador.php
+++ controladores/blog.controlador.php
@@ -81,13 +81,71 @@ class ControladorBlog{
 			   preg_match('/^[^0-9][a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[@][a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{2,4}$/', $_POST["correo_opinion"]) &&
 			   preg_match('/^[=\\$\\;\\*\\"\\?\\¿\\!\\¡\\:\\.\\,\\0-9a-zA-ZáéíóúÁÉÍÓÚñÑ ]+$/',  $_POST["contenido_opinion"])){
 
+
+				/********************************
+				 * VALIDACION FOTO LADO SERVIDOR
+				 * *******************************/
+				if(isset($_FILES['fotoOpinion']['tmp_name']) && !empty($_FILES['fotoOpinion']['tmp_name'])){
+					/********************************************************************
+				 * CAPTURAR ANCHO Y ALTO ORIGINAL DE LA IMAGEN Y DEFINIR NUEVOS VALORES
+				 * *********************************************************************/
+					list($ancho, $alto) = getimagesize($_FILES['fotoOpinion']['tmp_name']);
+
+					$nuevoAncho = 128;
+					$nuevoAlto = 128;
+
+
+				/****************************************
+				 *CREAMOS DIRECTORIO DONDE GUARDAR IMAGEN
+				 * ***************************************/
+
+				 $directorio = "vistas/img/usuarios/";
+				/********************************************************************
+				 * DE A CUERDO AL TIPO DE IMAGEN FUNCIONES DE PHP ESPECIFICAS
+				 * *********************************************************************/
+				if($_FILES['fotoOpinion']['type'] = "image/jpeg"){
+
+					$aleatorio = mt_rand(100,9999);
+					$ruta = $directorio.$aleatorio.'.jpg';
+
+					$origen = imagecreatefromjpeg($_FILES['fotoOpinion']['tmp_name']);//crea imagen
+					$destino = imagecreatetruecolor($nuevoAncho, $nuevoAlto);
+
+					imagecopyresized($destino, $origen, 0,0,0,0, $nuevoAncho, $nuevoAlto, $ancho, $alto);
+
+					imagejpeg($destino, $ruta);
+
+
+
+				} elseif($_FILES['fotoOpinion']['type'] = "image/png"){
+
+					$aleatorio = mt_rand(100,9999);
+					$ruta = $directorio.$aleatorio.'.png';
+
+
+					$origen = imagecreatefrompng($_FILES['fotoOpinion']['tmp_name']);//crea imagen
+					$destino = imagecreatetruecolor($nuevoAncho, $nuevoAlto);
+					imagealphablending($destino, FALSE);
+					imagesavealpha($destino, TRUE);
+					imagecopyresized($destino, $origen, 0,0,0,0, $nuevoAncho, $nuevoAlto, $ancho, $alto);
+
+					imagepng($destino, $ruta);
+
+				} else{
+					return "error-formato";
+				}
+
+				} else{
+					$ruta = 'vistas/img/usuarios/default.png';				}
+
+
 				$tabla='opiniones';
 
 						$datos = array(
 							   "id_art" => $_POST["id_art"],
 							   "nombre_opinion" => $_POST["nombre_opinion"],
 							   "correo_opinion" => $_POST["correo_opinion"],
-							   "foto_opinion" => 'vistas/img/usuarios/default.png',
+							   "foto_opinion" => $ruta,
 							   "contenido_opinion" => $_POST["contenido_opinion"],
 							   "fecha_opinion"=> date('Y-m-d'),
 							    "id_adm" => 1
